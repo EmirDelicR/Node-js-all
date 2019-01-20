fcab9a35e274a4afa666a8dc54ee55ca7249d145
Emir <emirdelictbf@gmail.com>
2019-01-20 14:40:10 +0100

    Adding section fro modules and routes

--- 6-Routes-Models/app.js
+++ 6-Routes-Models/app.js
@@ -7,14 +7,14 @@ const errorController = require("./controllers/error");
 
 const app = express();
 
-const hostname = "127.0.0.1";
-const port = 3001;
-
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+
+const hostname = "127.0.0.1";
+const port = 3001;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
